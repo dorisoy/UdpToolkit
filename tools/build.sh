be85e44979cbc2e3469403ce6cb56d17bfe4ae27
timofei.myagkikh <t.myagkikh@protonmail.com>
2021-09-06 00:08:00 +0300

    fix source generator publishing

--- tools/build.sh
+++ tools/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-dotnet restore ./UdpToolkit.sln --no-cache && dotnet publish ./UdpToolkit.sln -c Release -o ./server_build
+dotnet restore ./UdpToolkit.sln --no-cache && dotnet publish ./UdpToolkit.sln -c Release -o ./build
