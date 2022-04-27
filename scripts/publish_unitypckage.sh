faaddf571b0772d779d63f4a186bc1ca2d23b5a7
Timofey Myagkikh <t.myagkikh@protonmail.com>
2022-04-28 00:14:17 +0300

    revert unity project changes

--- scripts/publish_unitypckage.sh
+++ scripts/publish_unitypckage.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+MODE="$1"
+
 # folders for managed part
 mkdir './unity_package'
 
 ./scripts/copy_dlls_to_unity.sh
 
+if [[ $MODE == "local" ]]
+then
 # build local
-# /Applications/Unity/Hub/Editor/2020.3.24f1/Unity.app/Contents/MacOS/Unity -quit -batchmode -nographics -projectPath ./src/unity/UdpToolkit.Unity/ -exportPackage "Assets/Plugins/UdpToolkit" "Assets/Scripts/UdpToolkit" "../../../unity_package/UdpToolkit.dev.unitypackage"
-
+/Applications/Unity/Hub/Editor/2020.3.24f1/Unity.app/Contents/MacOS/Unity -quit -batchmode -nographics -projectPath ./src/unity/UdpToolkit.Unity/ -exportPackage "Assets/Plugins/UdpToolkit" "Assets/Scripts/UdpToolkit" "../../../unity_package/UdpToolkit.dev.unitypackage"
+else
 # build
 /opt/Unity/Editor/Unity -quit -batchmode -nographics -projectPath ./src/unity/UdpToolkit.Unity/ -exportPackage "Assets/Plugins/UdpToolkit" "Assets/Scripts/UdpToolkit" "../../../unity_package/UdpToolkit.$PACKAGE_VERSION.unitypackage"
+fi
